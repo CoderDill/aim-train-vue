35cd9f72811fe775b70db1126ff67897ac7cdc12
CrappyDill <mddillon47@gmail.com>
2022-03-13 19:59:04 -0700

    Timer/Buttons working

--- src/components/AimRange.vue
+++ src/components/AimRange.vue
@@ -1,9 +1,11 @@
 <template>
-<div>
-  <button v-if="!isRunning" @click="startGame">Start</button>
-</div>
   <div class="aimRange">
     <h1>Score: {{ score }} Timer: {{ timerCount }}</h1>
+    <div v-if="isRunning" class="target" @click="addScore"></div>
+    <div class="btn-container" >
+    <button v-if="!isRunning && gameOver" @click="startGame" class="btn">Start</button>
+    <button v-if="!isRunning && !gameOver" @click="reset" class="btn">Reset</button>
+    </div>
   </div>
 </template>
 
@@ -16,14 +18,16 @@ export default {
   data() {
     return {
       score: 0,
-      timerCount: 5,
-      isRunning: false
+      timerCount: 60,
+      isRunning: false,
+      gameOver: true
     }
   },
   watch: {
     timerCount(newTime) {
       if (newTime === 0) {
         this.isRunning = false;
+        this.gameOver = false;
         clearInterval(this.interval)
       }
     }
@@ -31,18 +35,27 @@ export default {
   methods: {
     startGame() {
       this.isRunning = true;
+      this.gameOver = false;
       this.interval = setInterval(() => {
         this.timerCount--
       }, 1000);
     },
     addScore() {
       this.score++
+    },
+    reset() {
+      this.score = 0;
+      this.timerCount = 60;
+      this.gameOver = true;
     }
   }
 };
 </script>
 
 <style>
+h1 {
+  color: plum
+}
 .aimRange {
   position: relative;
   display: block;
@@ -54,5 +67,29 @@ export default {
   color: black;
   cursor: crosshair;
   overflow: hidden;
+}
+.btn {
+  background-color: #4CAF50; /* Green */
+  border: none;
+  color: white;
+  padding: 10px 32px;
+  text-align: center;
+  text-decoration: none;
+  font-size: 16px;
+  border-radius: 30px;
+}
+.btn-container {
+  margin: 0;
+  position: relative;
+  top: 50%;
+  -ms-transform: translateY(-50%);
+  transform: translateY(-50%);
+}
+.target {
+  height: 35px;
+  width: 35px;
+  border-radius: 50%;
+  color: lavender;
+  background-color: lavender;
 }
 </style>
